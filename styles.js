cf5cf303a73515a89ee110bef9e59aeec86c81e8
Nick Baugh <niftylettuce@gmail.com>
2016-01-03 17:49:32 -0500

    Removed styling

--- styles.js
+++ styles.js
@@ -4,8 +4,6 @@ const NAV_HEIGHT = NAV_BAR_HEIGHT + STATUS_BAR_HEIGHT;
 
 module.exports = {
   navBarContainer: {
-    backgroundColor: 'white',
-    paddingBottom: 5,
   },
   statusBar: {
     height: STATUS_BAR_HEIGHT,
@@ -19,16 +17,16 @@ module.exports = {
     position: 'absolute',
     left: 0,
     right: 0,
-    bottom: 7,
+    bottom: 0,
+    top: 0,
+    justifyContent: 'center',
     alignItems: 'center',
   },
   navBarButton: {
-    marginTop: 12,
   },
   navBarButtonText: {
     fontSize: 17,
     letterSpacing: 0.5,
-    marginTop: 12,
   },
   navBarTitleText: {
     fontSize: 17,
@@ -36,9 +34,6 @@ module.exports = {
     color: '#333',
     fontWeight: '500',
     position: 'absolute',
-    left: 0,
-    right: 0,
-    bottom: 7,
     textAlign: 'center',
   },
 };
